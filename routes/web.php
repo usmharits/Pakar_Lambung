8ce8f6ce3dd3a2ee2a2200cac40df78c80f25503
usmharits <dkrsawangan110813@gmail.com>
2026-05-04 08:14:09 +0700

    Tambah konfigurasi vercel dan rute migrate

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,17 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DiagnosaController;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', [DiagnosaController::class, 'index']);
 Route::post('/diagnosa', [DiagnosaController::class, 'hitung'])->name('diagnosa.hitung');
+
+Route::get('/setup-database', function () {
+    try {
+        Artisan::call('migrate', ['--force' => true]);
+        Artisan::call('db:seed', ['--force' => true]);
+        return 'Alhamdulillah, Migrasi & Seeding Sukses!';
+    } catch (\Exception $e) {
+        return 'Error: ' . $e->getMessage();
+    }
+});
